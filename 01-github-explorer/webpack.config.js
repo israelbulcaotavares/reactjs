4e8f1d3964a2f338c37befa832f10c9602acb3ba
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-04-11 19:31:06 -0300

    Utilizando source maps

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode :'development',
+    devtool: 'eval-source-map', 
     entry: path.resolve(__dirname, 'src', 'index.jsx'), 
     output:{
         path: path.resolve(__dirname, 'dist'),
@@ -11,6 +12,9 @@ module.exports = {
     resolve:{
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+      },
     plugins:[
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
