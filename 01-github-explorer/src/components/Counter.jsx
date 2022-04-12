140e34fcec6b2191ecb1f7e6a69348ec8044d887
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-04-11 23:16:43 -0300

    Fast Refresh no Webpack

--- 01-github-explorer/src/components/Counter.jsx
+++ 01-github-explorer/src/components/Counter.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 //hook => Gancho
 
 
-export function Counter() { 
+export function Counter() {
     const [counter, setCounter] = useState(0);
 
 
 
     function increment() {
-        setCounter(counter + 1)
+        setCounter(counter + 1) //imutabilidade
     }
 
 
     return (
         <div>
             <h2>{counter}</h2>
-            <button type="button" onClick={increment}>Increment</button>
+            <button type="button" onClick={increment}>Increment +1</button>
         </div>
     );
 }
